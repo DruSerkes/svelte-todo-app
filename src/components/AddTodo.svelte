4b673295df962973c6afca58a9f690f14d325d94
Dru Serkes <dru@zephyrx.com>
2021-08-23 13:55:54 -0400

    update styles, add clearing

--- src/components/AddTodo.svelte
+++ src/components/AddTodo.svelte
@@ -1,21 +1,34 @@
 <script>
   export let addTodo;
+
   let currentTodo;
 
   const handleSubmit = () => {
+    if (!currentTodo) return;
     addTodo(currentTodo);
     currentTodo = "";
   };
+  const handleClear = (e) => if (e.key === "Escape") currentTodo = "";
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
-  <input type="text" bind:value={currentTodo} />
+  <input type="text" on:keyup={handleClear} bind:value={currentTodo} />
   <button type="submit">Add Todo</button>
 </form>
 
 <style>
+  input {
+    height: 1.75rem;
+  }
   button {
     background-color: cornflowerblue;
+    border: none;
     border-radius: 4px;
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+  }
+  button:hover {
+    background-color: darkblue;
+    color: whitesmoke;
   }
 </style>
